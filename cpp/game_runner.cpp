4943019557aaedbd21dfc2896af1bf61e076dae9
mincoding <74457149+mincoding1@users.noreply.github.com>
2024-04-23 15:09:16 +0900

    Update game_runner.cpp

--- cpp/game_runner.cpp
+++ cpp/game_runner.cpp
@@ -12,16 +12,12 @@ int main()
 
 	do {
 		aGame.rolling();
-		//rolling 역할 1 : 주사위를 굴린다.
-		//rolling 역할 2 : 퀴즈 카드를 한장 뽑는다.		
-		
-		//rolling 함수에서는 말의 위치(place)만 갱신한다.		
-		//answer 함수에서는 지갑, 코인, 페널티박스 상태값들을 갱신한다.
+
 		if (rand() % 9 == 7) {
-			notAWinner = aGame.wrongAnswer(); //퀴즈 오답처리 후, 감옥감, 턴을 넘김
+			notAWinner = aGame.wrongAnswer();
 		}
 		else {
-			notAWinner = aGame.wasCorrectlyAnswered(); //퀴즈 정답처리 후, 턴을 넘김
+			notAWinner = aGame.wasCorrectlyAnswered();
 		}
 	} while (notAWinner);
 }
