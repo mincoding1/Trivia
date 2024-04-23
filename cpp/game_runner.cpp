c759b669abe91867e12612585b6e4e68d9acd5cf
mincoding <74457149+mincoding1@users.noreply.github.com>
2024-04-23 10:13:20 +0900

    Update and rename GameRunner.cpp to game_runner.cpp

---
+++ cpp/game_runner.cpp
@@ -0,0 +1,25 @@
+#include "game.h"
+
+static bool notAWinner;
+
+int main()
+{
+	Game aGame;
+
+	aGame.add("Chet");
+	aGame.add("Pat");
+	aGame.add("Sue");
+
+	do {
+		aGame.roll(rand() % 5 + 1);
+
+		if (rand() % 9 == 7) {
+			notAWinner = aGame.wrongAnswer();
+		}
+		else {
+			notAWinner = aGame.wasCorrectlyAnswered();
+		}
+
+	} while (notAWinner);
+
+}
